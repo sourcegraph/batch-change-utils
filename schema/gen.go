a8bbd1f68a93ba2d885000219ffc3cb1b896a006
Adam Harvey <adam@adamharvey.name>
2020-09-29 16:40:30 -0700

    Remove schema.

--- schema/gen.go
+++
@@ -1,4 +0,0 @@
-package schema
-
-//go:generate env GO111MODULE=on go run stringdata.go -i campaign_spec.schema.json -name CampaignSpecJSON -pkg schema -o campaign_spec_stringdata.go
-//go:generate gofmt -s -w campaign_spec_stringdata.go
